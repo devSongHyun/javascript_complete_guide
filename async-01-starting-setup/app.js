25c4f8914183f1b7ab09f995fe348d0b4db2ce51
songhyun lee <thddkok.wk@gmail.com>
2024-01-03 19:35:36 +0900

    프로미스 시작하기

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,22 +1,32 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = (duration) => {
+  // 내장 API를 프로미스화
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
       // 바깥의 위치 구하는 콜백 함수가 실행되어야 타이머 실행
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (error) => {
       console.log(error);
     }
   );
   // 실행 순서 - 2: 타이머는 0초이더라도 호출 스택에 존재하는 console.log가 완료되어야지만 실행할 수 있다.
-  setTimeout(() => {
+  setTimer(1000).then(() => {
     console.log("Timer done!");
-  }, 0);
+  });
   // Track Me!를 눌렀을 때 자바스크립트의 방해 없이 바로 실행
   // 실행 순서 - 1
   console.log("Getting position...");
