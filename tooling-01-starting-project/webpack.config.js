af7e5a3f33626fae4b1091c5f600ba741fbb7dda
songhyun lee <thddkok.wk@gmail.com>
2024-01-07 18:28:42 +0900

    webpack-dev-서버 사용하기

--- tooling-01-starting-project/webpack.config.js
+++ tooling-01-starting-project/webpack.config.js
@@ -7,5 +7,8 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'), // __dirname: 현재 경로에 대한 액세스
     publicPath: 'assets/scripts/'
-  }
+  },
+  // devServer: {
+  //   contentBase: './'
+  // }
 };
