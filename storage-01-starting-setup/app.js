a85b1417fe37edcf0dd0974ad43ff2d5ef24a955
songhyun lee <thddkok.wk@gmail.com>
2024-01-09 19:08:41 +0900

    IndexedDB로 작업하기

--- storage-01-starting-setup/app.js
+++ storage-01-starting-setup/app.js
@@ -1,10 +1,17 @@
 const storeBtn = document.getElementById("store-btn");
 const retrBtn = document.getElementById("retrieve-btn");
 
+let db;
+
 const dbRequest = indexedDB.open("StorageDummy", 1);
 
+dbRequest.onsuccess = function (event) {
+  db = event.target.result;
+};
+
+// onupgradeneeded: 데이터베이스가 처음 만들어질 때 또는 버전이 변경될 때만 실행
 dbRequest.onupgradeneeded = function (event) {
-  const db = event.target.result;
+  db = event.target.result;
 
   const objStore = db.createObjectStore("products", { keyPath: "id" });
 
@@ -25,6 +32,28 @@ dbRequest.onerror = function () {
   console.log("ERROR!");
 };
 
-storeBtn.addEventListener("click", () => {});
+storeBtn.addEventListener("click", () => {
+  if (!db) {
+    return;
+  }
+  const productsStore = db
+      .transaction("products", "readwrite")
+      .objectStore("products");
+    productsStore.add({
+      id: "p2",
+      title: "A Second Product",
+      price: 122.99,
+      tags: ["Expensive", "Luxury"],
+    });
+});
 
-retrBtn.addEventListener("click", () => {});
+retrBtn.addEventListener("click", () => {
+  const productsStore = db
+      .transaction("products", "readwrite")
+      .objectStore("products");
+  const request = productsStore.get("p2");
+
+  request.onsuccess = function() {
+    console.log(request.result);
+  }
+});
