83338ab4a239da7fd8f8650c76afb48bed916dc5
songhyun lee <thddkok.wk@gmail.com>
2024-02-01 21:52:22 +0900

    주소 찾기 & 좌표 얻기

--- practice-places-01-starting-setup/src/SharePlace.js
+++ practice-places-01-starting-setup/src/SharePlace.js
@@ -1,5 +1,6 @@
-import { Modal } from './UI/Modal';
-import { Map } from './UI/Map';
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+import { getCoordsFromAddress } from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
@@ -7,7 +8,7 @@ class PlaceFinder {
     const locateUserBtn = document.getElementById("locate-btn");
 
     locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
-    addressForm.addEventListener("click", this.findAddressHandler.bind(this));
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
   }
 
   selectPlace(coordinates) {
@@ -25,7 +26,10 @@ class PlaceFinder {
       );
       return;
     }
-    const modal = new Modal('loading-modal-content', 'Loading, location - please wait');
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading, location - please wait"
+    );
     modal.show();
     navigator.geolocation.getCurrentPosition(
       (successResult) => {
@@ -46,7 +50,26 @@ class PlaceFinder {
     );
   }
 
-  findAddressHandler() {}
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector("input").value;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address entered - please try again!");
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading, location - please wait"
+    );
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates);
+    } catch (err) {
+      alert(err.message);
+    }
+    modal.hide();
+  }
 }
 
 const placeFinder = new PlaceFinder();
