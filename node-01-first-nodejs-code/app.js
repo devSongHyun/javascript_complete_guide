9d07e5d7d86b0569483d4b5fb86d2a81d0eb45bc
songhyun lee <thddkok.wk@gmail.com>
2024-02-29 19:18:51 +0900

    들어오는 데이터 구문 분석하기

--- node-01-first-nodejs-code/app.js
+++ node-01-first-nodejs-code/app.js
@@ -1,9 +1,23 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-  response.setHeader('Content-TYpe', 'text/html');
-  response.write('<h1>Hello there!</h1>');
-  response.end();
+  let body = [];
+  // on - 리스너 추가
+  request.on("data", (chunk) => {
+    body.push(chunk);
+  });
+  request.on("end", () => {
+    body = Buffer.concat(body).toString();
+    let userName = "Unknown user";
+    if (body) {
+      userName = body.split("=")[1];
+    }
+    response.setHeader("Content-TYpe", "text/html");
+    response.write(
+      `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
+    );
+    response.end();
+  });
 });
 
 server.listen(3000);
