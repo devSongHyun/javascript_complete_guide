9d35f40406616e3a2ca03f77606933cc934c20bb
songhyun lee <thddkok.wk@gmail.com>
2024-02-29 18:56:07 +0900

    들어오는 HTTP 요청으로 작업하기

--- node-01-first-nodejs-code/app.js
+++ node-01-first-nodejs-code/app.js
@@ -1,17 +1,8 @@
-const fs = require('fs');
+const http = require('http');
 
-fs.readFile('user-data.txt', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data.toString());
-})
-
-fs.writeFile('user-data.txt', 'username=Max', err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Wrote to file!')
-  }
+const server = http.createServer((request, response) => {
+  response.write('hello there!');
+  response.end();
 });
+
+server.listen(3000);
