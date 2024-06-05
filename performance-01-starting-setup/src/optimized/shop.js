89d4bda7fcdd0d9c1c1f3a5c384082eccde5cb89
shlee <shlee@gdnps.com>
2024-06-05 15:25:52 +0900

    최적화 시작 시간 & 코드 사용 / 범위

---
+++ performance-01-starting-setup/src/optimized/shop.js
@@ -0,0 +1,24 @@
+import { products } from './products';
+import { renderProducts } from './rendering';
+
+const addProduct = (event) => {
+  import('./product-management.js').then(mod => {
+    mod.addProduct(event);
+  })
+}
+
+const deleteProduct = (productId) => {
+  import('./product-management.js').then(mod => {
+    mod.deleteProduct(productId);
+  })
+}
+
+function initProducts() {
+  renderProducts(products, deleteProduct);
+}
+
+const addProductForm = document.getElementById('new-product');
+
+initProducts();
+
+addProductForm.addEventListener('submit', addProduct);
